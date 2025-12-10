08da69540c6b03a47450529c5968516410b2d79b
TushR1 <154993025+TushR1@users.noreply.github.com>
2025-12-10 20:00:37 +0530

    Add files via upload
    This is my first assignment submission

---
+++ task1.py
@@ -0,0 +1,38 @@
+"""
+Write a Python program that does the following:
+1.  Takes two numbers as input from the user.
+2.  Performs the basic mathematical operations on these two numbers:
+o	Addition
+o	Subtraction
+o	Multiplication
+o	Division
+3.  Displays the results of each operation on the screen.
+
+"""
+
+#Addition
+a = input("Enter Value of a : ")
+b = input("Enter Value of b : ")
+
+
+c = a+b
+print("Addition of both a and b is : ", c)
+
+
+#Subtractionon
+d = a+b
+print("Subtractionon of both a and b is : ", d)
+
+
+#Multiplication
+e = a+b
+print("Multiplication of both a and b is : ", e)
+
+
+#Division
+f = a+b
+print("Division of both a and b is : ", f)
+
+
+
+
---
+++ task2.py
@@ -0,0 +1,13 @@
+"""
+Write a Python program that:
+1.  Takes a user's first name and last name as input.
+2.  Concatenates the first name and last name into a full name.
+3.  Prints a personalized greeting message using the full name
+
+"""
+
+f_name = input("Enter your First name :")
+l_name = input("Enter Your Last name: ")
+
+
+print("Hello,", f_name + " " + l_name + "!" + " " + "Welcome to the python program.")
